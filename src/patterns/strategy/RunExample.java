f27e622efeda9928f9472eb0eb195f40d1f6b5c6
Bekzat Muratov <bekzatmuratov@users.noreply.github.com>
2026-05-19 17:12:57 +0500

    Simplify repository structure and documentation

--- src/patterns/strategy/RunExample.java
+++
@@ -1,9 +0,0 @@
-public class RunExample {
-    public static void main(String[] args) {
-        ScoreCounter normal = new ScoreCounter(new NormalScore());
-        ScoreCounter fast = new ScoreCounter(new FastScore());
-
-        System.out.println("Обычные очки: " + normal.count(100));
-        System.out.println("Быстрая езда: " + fast.count(100));
-    }
-}
